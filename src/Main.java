1db7f19a278ce0b750395e7092f17ecbdea0c1e8
MonicaPoveda <monicapoveda337@gmail.com>
2025-02-21 15:41:00 -0500

    se guardan cambios en main

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("mi primer linea de texto");
         System.out.println("linea creada en la rama2 para subir a main");
-        System.out.println("escribo para hacer el git fetch--all");
     }
 }
